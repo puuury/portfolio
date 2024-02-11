3469c5e9a47e2e3536b0dbe3c1133579367bf3a6
Mr.Tamry <pouryatamri9@gmail.com>
2024-02-11 12:03:39 +0330

    first upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "WordPress Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+var typed = new Typed(".text2", {
+  strings: ["Tamry."],
+  typeSpeed: 500,
+  // backSpeed: 500,
+  // backDelay: 10000,
+  loop: false
+});
+
